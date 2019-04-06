be6e18d53898cd618ef5e2cdd3e1accd6b9056f9
imanzal <ansalkhan.k.a@lxisoft.com>
2019-04-06 11:13:57 +0530

    slides

--- src/app/pages/day/day.page.ts
+++ src/app/pages/day/day.page.ts
@@ -1,5 +1,5 @@
-import { NavParams } from '@ionic/angular';
-import { Component, OnInit } from '@angular/core';
+import { NavParams, IonSlides } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Route, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
@@ -9,11 +9,23 @@ import { Route, ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class DayPage implements OnInit {
   day: String;
+  @ViewChild('slides') slides: IonSlides;
   tasks = [ 'Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5' ];
+  roles = [ 'Role 1', 'Role 2', 'Role 3' ];
+  slideOpts = {
+    effect: 'flip'
+  };
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
    this.day = this.route.snapshot.paramMap.get('day');
   }
 
+  slideNext() {
+    this.slides.slideNext();
+  }
+
+  slidePrev() {
+    this.slides.slidePrev();
+  }
 }
