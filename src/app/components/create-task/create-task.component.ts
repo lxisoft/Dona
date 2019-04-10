c6696f85bc81bce23e5118a0b14d67207da33430
imanzal <ansalkhan.k.a@lxisoft.com>
2019-04-10 14:29:48 +0530

    Adding task style changed

--- src/app/components/create-task/create-task.component.ts
+++ src/app/components/create-task/create-task.component.ts
@@ -1,7 +1,8 @@
 import { Task } from './../../models/task';
 import { Day } from './../../models/day';
 import { DaysService } from './../../services/days.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-task',
@@ -13,17 +14,35 @@ export class CreateTaskComponent implements OnInit {
   day: Day;
   tasks: Task[] = [];
   task: Task;
-  constructor() {
+  placeholder= 'Create new task';
+  constructor(private modalController: ModalController) {
    }
 
   ngOnInit() {
     this.task = new Task('', this.day.day);
   }
 
-  addTask() {
-    this.tasks.push(this.task);
-    this.day.tasks.push(this.tasks[this.tasks.length - 1]);
-    this.task = new Task('', this.day.day);
+  addTask(): boolean {
+    const regex = /[\w]+/;
+    if (!(this.task.detail === null || this.task.detail === undefined || this.task.detail === '')) {
+      if (this.task.detail.match(regex) !== null) {
+        this.tasks.push(this.task);
+        this.day.tasks.push(this.tasks[this.tasks.length - 1]);
+        this.task = new Task('', this.day.day);
+        return true;
+      } else {
+        this.task.detail = '';
+        this.placeholder = 'Invalid content';
+        return false;
+      }
+    }
+    return true;
+  }
+
+  dismissModal(force: boolean) {
+    if (force || this.addTask()) {
+      this.modalController.dismiss();
+    }
   }
 
 }
